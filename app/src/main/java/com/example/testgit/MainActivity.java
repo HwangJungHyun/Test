820c3a362ce16e1da42171ea000d09b49f4d3622
JungHyun <f7817455@naver.com>
2020-08-10 15:50:01 +0900

    test

---
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.testgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button testBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testBtn = findViewById(R.id.test_bnt);
+
+        testBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
